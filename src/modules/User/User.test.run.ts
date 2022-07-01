cc15076dd557a086310b985cf3fa7cee369fe668
Gabriel Barros Feitosa Sá <gabreilbarros13@gmail.com>
2022-07-01 17:07:03 -0300

    Create infra
    Create infra with express

--- src/modules/User/User.test.run.ts
+++ src/modules/User/User.test.run.ts
@@ -1,12 +1,10 @@
 import { CreateUser } from './Services/CreateUser';
-import { v4 as uuidv4 } from 'uuid';
 
 (async () => {
 
   const createUser = new CreateUser();
 
   await createUser.create({
-    id: `${uuidv4()}`,
     idUserPermission: 1,
     Name: 'Franncy Neya Brasil Feitosa',
     Birthday: '07-10-1978',
